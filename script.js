35a47806da28bc1d0db21125e73b1dc974a04c11
Caleb Ford <C00600410@louisiana.edu>
2026-02-24 04:11:11 +0000

    Finish

--- script.js
+++ script.js
@@ -2,34 +2,48 @@
 const form = document.getElementById("checkInForm");
 const nameInput = document.getElementById("attendeeName");
 const teamInput = document.getElementById("teamSelect");
+const greet = document.getElementById("greeting");
+const progressBar = document.getElementById("progressBar");
+const attendanceCount = document.getElementById("attendeeCount");
 
 //Track attendance
 let count = 0;
 const maxCount = 50;
 //Handle form submission
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    const name = nameInput.value;
-    const team = teamInput.value;
-    const teamName = teamInput.selectedOptions[0].text;
+  const name = nameInput.value;
+  const team = teamInput.value;
+  const teamName = teamInput.selectedOptions[0].text;
 
-     console.log(name, team, teamName);
+  console.log(name, team, teamName);
 
-     //Increment count
-     count++;
-     console.log(count);
-     
-     //Update progress bar
-     const percentage = Math.round((count / maxCount) * 100) + "%"; 
-    console.log(percentage);
+  //Increment count
+  count++;
+  attendanceCount.textContent = count;
+  console.log(count);
 
-    const teamCounter = document.getElementById(team + "Count");
-    console.log(teamCounter);
-    teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
+  //Update progress bar
+  const percentage = Math.round((count / maxCount) * 100) + "%";
+  console.log(percentage);
 
-    const message = `:surprise: Welcome, ${name} from ${teamName}!`;
-    
-    form.reset();
+  const teamCounter = document.getElementById(team + "Count");
+  console.log(teamCounter);
+  teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
 
+  const message = `🎉 Welcome, ${name} from ${teamName}!`;
+
+  greet.textContent = message;
+
+  greet.style.display = "block";
+
+  const teamCard = document.querySelector(".team-card." + team);
+
+  greet.style.backgroundColor =
+    window.getComputedStyle(teamCard).backgroundColor;
+
+  progressBar.style.width = percentage;
+
+  form.reset();
 });
